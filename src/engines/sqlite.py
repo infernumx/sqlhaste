a2d186298843a4ae97ed3860487dfda2bc6bc2fb
infernumx <infernumx@protonmail.com>
2021-10-13 12:18:25 -0700

    Removed unnecessary optional type hints

--- src/engines/sqlite.py
+++ src/engines/sqlite.py
@@ -12,10 +12,12 @@ class SQLiteManager(EngineBase):
     def execute(self, query: str, *args, fetch_all=True) -> SQLResults | SQLResult:
         with self.connection as cursor:
             result: Cursor = cursor.execute(query, args)
-            return result.fetchall() if fetch_all else result.fetchone()
+            if fetch_all:
+                return result.fetchall()
+            return result.fetchone()
 
-    def get_table_names(self) -> SQLResults | SQLResult:
+    def get_table_names(self) -> SQLResults:
         return self.execute("SELECT name FROM sqlite_master WHERE type='table'")
 
-    def get_rows_by_table_name(self, table_name: str) -> SQLResults | SQLResult:
+    def get_rows_by_table_name(self, table_name: str) -> SQLResults:
         return self.execute(f"SELECT * FROM {table_name}")
