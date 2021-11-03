f8e14191676c2c40f11e89c887fa39e7665bfaa1
infernumx <infernumx@protonmail.com>
2021-11-03 07:23:03 -0700

    Allow for creation of new databases

--- sqlhaste.py
+++ sqlhaste.py
@@ -3,6 +3,11 @@
 from src import DatabaseManager
 from rich.console import Console
 import sys
+import glob
+
+
+def get_db_files() -> list[str]:
+    return glob.glob("*.db")
 
 
 def main() -> None:
@@ -10,7 +15,13 @@ def main() -> None:
         # Ask for database name and engine since they were not passed as arguments
         console: Console = Console()
         console.rule("SQLHaste")
-        db_name: str = console.input(">> Input database name to open: ")
+        # print all available databases
+        console.print("[underline]Available databases:[/underline]")
+        for db in get_db_files():
+            console.print(f"[yellow]{db}[/yellow]")
+        db_name: str = console.input(
+            ">> Input database name to open, or type a new database name to create one: "
+        )
         db_engine: str = console.input(
             ">> Input database engine to use (SQLite, MySQL, PostgreSQL): "
         )
