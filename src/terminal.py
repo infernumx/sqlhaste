1dc27457ecff315168774dac6fd067133415b9ff
infernumx <infernumx@protonmail.com>
2021-11-16 08:53:27 -0700

    Update execute method

--- src/terminal.py
+++ src/terminal.py
@@ -221,5 +221,5 @@ class Terminal:
                 break
             columns.append(column)
 
-        self.engine.execute(f"CREATE TABLE {table_name} ({', '.join(columns)})")
+        self.engine.execute_get_one(f"CREATE TABLE {table_name} ({', '.join(columns)})")
         self.success("Table created!")
